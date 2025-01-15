1eed3c97b21bcbe2d6876b73fd4d324d21569381
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-15 19:40:02 +0100

    1. HTTP - Request Object

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const http = require('http')
+const http = require("http")
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
+  // Checking information on type of request made and the path requested
+  console.log(req.method)
+  console.log(req.url)
 
-res.writeHead(200,{'content-type': 'text/html'})
-res.write('<h1>Homepage</h1>')
-res.end()
+  const url = req.url
 
+  if (url === "/") {
+    // Homepage
+    res.writeHead(200, { "content-type": "text/html" })
+    res.write("<h1>Homepage</h1>")
+    res.end()
+  }
+//   About Page
+  else if(url === '/about'){
+    res.writeHead(200, { "content-type": "text/html" })
+    res.write("<h1>About Page</h1>")
+    res.end()
+  }
+  else{
+    // 404
+    res.writeHead(404, { "content-type": "text/html" })
+    res.write("<h1>Page not found</h1>")
+    res.end()
+  }
 })
 
 server.listen(5000)
