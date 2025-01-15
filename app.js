b42dc84b51e19bca5fc55443483ee3c0f8e80cd5
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-15 19:10:21 +0100

    1. HTTP basics

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+
+console.log('user hit the server ');
+
+res.end('Home page response')
+
+})
+
+server.listen(5000)
