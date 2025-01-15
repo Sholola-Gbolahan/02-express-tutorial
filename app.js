d6a8b9264409383fb02f9e119b1bfcba20502867
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-15 20:08:58 +0100

    1. HTTP - App samples setup

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const {readFileSync} = require('fs')
 
 // get all files
 // readFileSync is called only once when the first request is made.
-const homepage = readFileSync('./index.html');
+const homepage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
+const homeImage = readFileSync('./navbar-app/logo.svg');
 
 const server = http.createServer((req, res) => {
   // Checking information on type of request made and the path requested
-  console.log(req.method)
   console.log(req.url)
 
   const url = req.url
@@ -22,6 +24,24 @@ const server = http.createServer((req, res) => {
   else if(url === '/about'){
     res.writeHead(200, { "content-type": "text/html" })
     res.write("<h1>About Page</h1>")
+    res.end()
+  }
+//   Styles
+  else if(url === '/styles.css'){
+    res.writeHead(200, { "content-type": "text/css" })
+    res.write(homeStyles)
+    res.end()
+  }
+  //   image/Logo
+  else if(url === '/logo.svg'){
+    res.writeHead(200, { "content-type": "image/svg+xml" })
+    res.write(homeImage)
+    res.end()
+  }
+  //   Logic
+  else if(url === '/browser-app.js'){
+    res.writeHead(200, { "content-type": "text/javascript" })
+    res.write(homeLogic)
     res.end()
   }
   else{
