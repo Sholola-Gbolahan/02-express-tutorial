726bd1c5b0199f82175fa9f06917ad04f050e2ad
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-16 14:41:47 +0100

    3.1 JSON - Basic - setting up files

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require("express")
-const path = require("path")
 
 const app = express()
 
-// Setup static and middleware
-// Static mean is a file server doesn't have to change
-app.use(express.static('./public'))
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
-
-// adding to static assets
-// SSR
-// })
-
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found")
+app.get("/", (req, res) => {
+  res.json([
+    {
+      name: 'John',
+    },
+    {
+      name: 'Susan'
+    },
+  ])
 })
 
+
+
 app.listen(5000, () => {
-  console.log("Server is listen from  port : 5000")
+  console.log("server listening on port : 5000")
 })
