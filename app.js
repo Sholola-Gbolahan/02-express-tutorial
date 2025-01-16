a9854ecbdf936842078ebd2fabc5c42125a8b411
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-16 14:58:56 +0100

    4. Params - Query String - setup - Filtering out what to be sent out

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const {products} = require('./data')
 const app = express()
 
 app.get("/", (req, res) => {
-  res.json(products)
+  res.send('<h1> Homepage</h1> <a href="api/products" > products</a>' )
 })
 
+app.get("/api/products", (req, res) => {
+    // sending back without adding description 
+    const newProducts = products.map((product) =>{
+        const {id,name,image} = product
+        return {id,name,image}
+    } )
+    res.send(newProducts)
+  })
+  
 app.listen(5000, () => {
   console.log("server listening on port : 5000")
 })
