310f5ec3b5edbd8125481d36a483ce079b3f1762
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-16 14:07:19 +0100

    2. Express - App Example Setup

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+// Setup static and middleware
+// Static mean is a file server doesn't have to change
+app.use(express.static('./public'))
+
 app.get("/", (req, res) => {
-  // we can pass ion string as well as HTML
-  // express uses send
-  console.log('User hit the server');
-  
-  res.status(200).send("Homepage")
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
 })
 
-app.get("/about", (req, res) => {
-  res.send("About Page")
-})
-
-
-// This handles all http verbs with any method
-app.all('*',(req,res)=>{
-    // status() is used to pass in the status code
-    res.status(404).send('<h1>Resource not found</h1>')
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found")
 })
 
 app.listen(5000, () => {
-  console.log("server is listen on port:5000")
+  console.log("Server is listen from  port : 5000")
 })
-
-// app.get - Read Data
-// app.post - Inster Data
-// app.put  - Update Data
-// app.delete - Delete Data
-// app.all
-// app.use
-// app.listen
