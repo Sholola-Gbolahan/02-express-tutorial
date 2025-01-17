923e40dd12627deea193591b2f806351bdf6086b
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-17 08:00:32 +0100

    5. Query String  - complex instance to filter products by parameters

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
 const express = require("express")
-const {products} = require('./data')
+const { products } = require("./data")
 
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send('<h1> Homepage</h1> <a href="api/products" > products</a>' )
+  res.send('<h1> Homepage</h1> <a href="api/products" > products</a>')
 })
 // Route parameter is setup to avoid hand coding products params
 app.get("/api/products/:productID", (req, res) => {
-  console.log(req);
-  console.log(req.params);
-  
-  const {productID} = req.params
-  // Converting productID from string to number 
-    const singleProduct = products.find((product)=> product.id === Number(productID))
-    // setting up 404 if params not exit
-    if(!singleProduct){
-      res.status(404).send('Product Does Not Exit')
-    }
-    console.log(singleProduct);
-    res.json(singleProduct);
-  })
+  console.log(req)
+  console.log(req.params)
+
+  const { productID } = req.params
+  // Converting productID from string to number
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  )
+  // setting up 404 if params not exit
+  if (!singleProduct) {
+    res.status(404).send("Product Does Not Exit")
+  }
+  console.log(singleProduct)
+  res.json(singleProduct)
+})
 
 // Complex Params
-  app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req);
-    console.log(req.params);
-    res.send('Hello World')
-  })
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req)
+  console.log(req.params)
+  res.send("Hello World")
+})
 
-  // Query strings setup
+// Query strings setup
 
-  app.get('/api/v1/query', (req,res)=>{
-    // query format is : http://localhost:5000/api/v1/query?name=john&age=32
-    console.log(req.query);
-    res.send('hello world')
+app.get("/api/v1/query", (req, res) => {
+  // query format is : http://localhost:5000/api/v1/query?name=john&age=32
 
-    
-  })
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+  console.log(req.query)
 
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
 
-    
-  
-  
-  
+  res.status(200).send(sortedProducts)
+
+  // res.send('hello world')
+})
+
 app.listen(5000, () => {
   console.log("server listening on port : 5000")
 })
