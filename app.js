5ef31e081cb52150b7e73126308f4b52e7af52a0
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-17 06:57:51 +0100

    4. Params - handcodedly getting product by params value

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ const app = express()
 app.get("/", (req, res) => {
   res.send('<h1> Homepage</h1> <a href="api/products" > products</a>' )
 })
-
-app.get("/api/products", (req, res) => {
-    // sending back without adding description 
-    const newProducts = products.map((product) =>{
-        const {id,name,image} = product
-        return {id,name,image}
-    } )
-    res.send(newProducts)
-  })
+// Route parameter is setup to avoid hand coding products params
+app.get("/api/products/1", (req, res) => {
+    const singleProduct = products.find((product)=> product.id === 1)
+    res.json(singleProduct)
+  })                                                  
   
 app.listen(5000, () => {
   console.log("server listening on port : 5000")
