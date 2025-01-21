aa98e0ac2549e336cda9fb4cd89b4d5d31b5e461
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-21 14:42:18 +0100

    6.Middleware  - Using middleware

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const { products } = require("./data")
 const app = express()
 //  req => middleware => res
 
-app.get("/", (req, res) => {
+const logger = (req, res, next) => {
   const method = req.method
   const url = req.url
   const time = new Date().getFullYear()
   console.log(method, url, time)
 
+  // terminating the whole cycle and sending the response
+  // res.send("Testing")
+
+  // Passing to the next middleware
+  next()
+}
+
+app.get("/", logger, (req, res) => {
   return res.send("Home")
 })
 
-app.get("/about", (req, res) => {
-  const method = req.method
-  const url = req.url
-  const time = new Date().getFullYear()
-  console.log(method, url, time)
+app.get("/about", logger, (req, res) => {
   return res.send("about")
 })
 
--- note.md
+++ note.md
@@ -1,2 +1,4 @@
 Middleware : They are functions that execute during the request to the server, each middleware functions has access to request and response object.
 Middleware: is every were in express.
+When you work with middleware, you must always pass to the next middleware
+Next() is used to pass to the nest middleware
