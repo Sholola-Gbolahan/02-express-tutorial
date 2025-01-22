ac29e984acfc79a8bef328ffa151bcb8149e999e
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-22 17:03:53 +0100

    8. POST - Having access to static files using epress middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const { people } = require("./data")
 const app = express()
 
+// Static assets
+app.use(express.static("./methods-public"))
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people })
 })
--- note.md
+++ note.md
@@ -4,6 +4,7 @@
   Middleware: is every were in express.
   When you work with middleware, you must always pass to the next middleware
   Next() is used to pass to the nest middleware
+- app.use : applies middlware to all our routes
 
 ## Middleware Options
 
