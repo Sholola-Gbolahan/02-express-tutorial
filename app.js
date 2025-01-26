507ede019b067e652dd5969fa9d401998c417997
Gbolahan <shololagbolahan@gmail.com>
2025-01-26 03:57:56 +0100

    10. PUT - Updating list of data base on Params

--- app.js
+++ app.js
@@ -37,6 +37,34 @@ app.post("/api/postman/people", (req, res) => {
   res.status(201).json({ success: true, data: [...people, name] })
 })
 
+// Updating Name
+app.put("/api/people/:id", (req, res) => {
+  // getting a particular data
+  const { id } = req.params
+  // getting value to update
+  const { name } = req.body
+
+  // checking if selected person id exit
+  const person = people.find((person) => person.id === Number(id))
+  // Returning error message if person not exit
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, data: `no person with id ${id}` })
+  }
+
+  // Creating a new list of people with the updated value
+  const newPeople = people.map((person) => {
+    // Updating any of the value that matches id
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    // return each list
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
 app.post("/login", (req, res) => {
   console.log(req.body)
   const { name } = req.body
