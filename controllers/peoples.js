00dc5423a580f973a92881a77906c1610aeb558b
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-28 13:30:31 +0100

    13. Creating a cleaner router using controllers

---
+++ controllers/peoples.js
@@ -0,0 +1,64 @@
+const { people } = require("../data")
+
+const getPeople = (req, res) => {
+  res.status(200).json({ success: true, data: people })
+}
+
+const createPerson = (req, res) => {
+  const { name } = req.body
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" })
+  }
+  res.status(201).json({ success: true, person: name })
+}
+
+const createPersonPostman = (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "pls provide name value" })
+  }
+
+  res.status(201).json({ success: true, data: [...people, name] })
+}
+
+const updatePerson = (req, res) => {
+  // getting a particular data
+  const { id } = req.params
+  // getting value to update
+  const { name } = req.body
+
+  // checking if selected person id exit
+  const person = people.find((person) => person.id === Number(id))
+  // Returning error message if person not exit
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, data: `no person with id ${id}` })
+  }
+
+  // Creating a new list of people with the updated value
+  const newPeople = people.map((person) => {
+    // Updating any of the value that matches id
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    // return each list
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+}
+
+const deletePerson = "Delete person"
+
+module.exports = {
+  getPeople,
+  createPerson,
+  updatePerson,
+  createPersonPostman,
+  deletePerson,
+}
--- routes/people.js
+++ routes/people.js
@@ -1,59 +1,21 @@
 const express = require("express")
 const router = express.Router()
-const { people } = require("../data")
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people })
-})
+const {
+  getPeople,
+  createPerson,
+  updatePerson,
+  createPersonPostman,
+  deletePerson,
+} = require("../controllers/peoples")
 
-router.post("/", (req, res) => {
-  const { name } = req.body
+router.get("/", getPeople)
 
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" })
-  }
-  res.status(201).json({ success: true, person: name })
-})
+router.post("/", createPerson)
 
-router.post("/postman", (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "pls provide name value" })
-  }
-
-  res.status(201).json({ success: true, data: [...people, name] })
-})
+router.post("/postman", createPersonPostman)
 
 // Updating Name
-router.put("/:id", (req, res) => {
-  // getting a particular data
-  const { id } = req.params
-  // getting value to update
-  const { name } = req.body
-
-  // checking if selected person id exit
-  const person = people.find((person) => person.id === Number(id))
-  // Returning error message if person not exit
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, data: `no person with id ${id}` })
-  }
-
-  // Creating a new list of people with the updated value
-  const newPeople = people.map((person) => {
-    // Updating any of the value that matches id
-    if (person.id === Number(id)) {
-      person.name = name
-    }
-    // return each list
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
-})
+router.put("/:id", updatePerson)
 
 module.exports = router
